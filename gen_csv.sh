d94c6713972d9b8ac23108d7959a73784ec56fef
chengjia37 <80321860+chengjia37@users.noreply.github.com>
2021-03-09 19:35:46 +0900

    Update gen_csv.sh

--- gen_csv.sh
+++ gen_csv.sh
@@ -3,10 +3,10 @@
 #while [ true ]
 #do
     PREFIX=`date '+%Y%m%d'`
-    SHOPID=-100
+    SHOPID=-500
     POSID=1
     FILENAME=${PREFIX}_SHOP${SHOPID}_POS${POSID}.csv
-    if [ `uname` != "Darwin" ]; then
+    if [ `uname` = "Darwin" ]; then
         gen_csv/gen_csv.exe -shopid ${SHOPID} -posid ${POSID} > data/before/${FILENAME}
     else
         go run gen_csv/gen_csv.go -shopid ${SHOPID} -posid ${POSID} > data/before/${FILENAME}
